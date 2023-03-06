a2fd93aa4221cf9a48e6ca504e6c903ad2f4436c
omgzsa <zselloandras@gmail.com>
2023-03-06 20:28:04 +0100

    refactoring hero

--- frontend/src/components/home/HomeHero.vue
+++ frontend/src/components/home/HomeHero.vue
@@ -14,42 +14,45 @@ defineProps({
 <template>
   <carousel class="text-center py-8">
     <slide v-for="slide in slides" :key="slide.alt">
-      <div class="carousel__item">
-        <div class="flex flex-col mx-auto text-center justify-start">
-          <div class="px-2 max-w-2xl mx-auto">
-            <h1>{{ slide.title }}</h1>
-            <p class="mb-8 mt-auto">{{ slide.text }}</p>
-            <!-- BUTTONS -->
-            <div class="flex flex-col items-center md:mx-auto md:flex-row mb-8">
-              <RouterLink :to="{ name: 'services', path: '/services' }">
-                <button
-                  class="rounded-md px-8 py-2 m-1 overflow-hidden relative group cursor-pointer font-medium text-white bg-accent-100 border border-accent-100"
+      <div
+        class="carousel__item w-full flex justify-center place-items-center bg-contain bg-no-repeat bg-bottom"
+        :style="{ backgroundImage: `url(${useGetImageUrl(slide.img)})` }"
+      >
+        <div
+          class="container mx-auto flex flex-col items-center px-4 py-16 text-center md:py-32 md:px-10 lg:px-32 xl:max-w-3xl"
+        >
+          <h1>{{ slide.title }}</h1>
+          <p class="mb-8 mt-auto">{{ slide.text }}</p>
+          <!-- BUTTONS -->
+          <div class="flex flex-col items-center md:mx-auto md:flex-row mb-8">
+            <RouterLink :to="{ name: 'services', path: '/services' }">
+              <button
+                class="rounded-md px-8 py-2 m-1 overflow-hidden relative group cursor-pointer font-medium text-white bg-accent-100 border border-accent-100"
+              >
+                <span
+                  class="absolute w-64 h-0 transition-all duration-300 origin-center rotate-45 -translate-x-20 bg-white top-1/2 group-hover:h-64 group-hover:-translate-y-32 ease"
+                ></span>
+                <span
+                  class="flex flex-row gap-2 relative text-white transition duration-300 group-hover:text-accent-100 ease"
+                  >Szolgáltatások</span
                 >
-                  <span
-                    class="absolute w-64 h-0 transition-all duration-300 origin-center rotate-45 -translate-x-20 bg-white top-1/2 group-hover:h-64 group-hover:-translate-y-32 ease"
-                  ></span>
-                  <span
-                    class="flex flex-row gap-2 relative text-white transition duration-300 group-hover:text-accent-100 ease"
-                    >Szolgáltatások</span
-                  >
-                </button>
-              </RouterLink>
-              <RouterLink :to="{ name: 'about', path: '/about' }">
-                <button
-                  class="rounded-md px-8 py-2 m-1 overflow-hidden relative group cursor-pointer border font-medium border-accent-100 text-accent-100"
+              </button>
+            </RouterLink>
+            <RouterLink :to="{ name: 'about', path: '/about' }">
+              <button
+                class="rounded-md px-8 py-2 m-1 overflow-hidden relative group cursor-pointer border font-medium border-accent-100 text-accent-100"
+              >
+                <span
+                  class="absolute w-64 h-0 transition-all duration-300 origin-center rotate-45 -translate-x-20 bg-accent-100 top-1/2 group-hover:h-64 group-hover:-translate-y-32 ease"
+                ></span>
+                <span
+                  class="flex flex-row gap-2 relative text-accent-100 transition duration-300 group-hover:text-white ease"
+                  >Időpontfoglalás</span
                 >
-                  <span
-                    class="absolute w-64 h-0 transition-all duration-300 origin-center rotate-45 -translate-x-20 bg-accent-100 top-1/2 group-hover:h-64 group-hover:-translate-y-32 ease"
-                  ></span>
-                  <span
-                    class="flex flex-row gap-2 relative text-accent-100 transition duration-300 group-hover:text-white ease"
-                    >Időpontfoglalás</span
-                  >
-                </button>
-              </RouterLink>
-            </div>
+              </button>
+            </RouterLink>
           </div>
-          <img :src="useGetImageUrl(slide.img)" :alt="slide.alt" />
+          <!-- <img :src="useGetImageUrl(slide.img)" :alt="slide.alt" /> -->
         </div>
       </div>
     </slide>
@@ -65,15 +68,14 @@ defineProps({
   padding-left: 0px;
 }
 
-.carousel__item {
+/* .carousel__item {
   min-height: 200px;
   width: 100%;
   display: flex;
   justify-content: center;
   align-items: center;
-
-  /* border: 1px solid rgb(40, 207, 187); */
-}
+  border: 1px solid rgb(40, 207, 187);
+} */
 
 .carousel__slide {
   /* padding: 10px; */
--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -5,18 +5,21 @@ import HomeHero from "../components/home/HomeHero.vue";
 const slides = ref([
   {
     title: "Lorem ipsum dolor sit amet",
+    alt: "mirrored image of the same woman, one is in black and white",
     text: "Lorem ipsum dolor sit amet consectetur. Magna faucibus sed est facilisis.",
-    img: "woman_hero_1.webp",
+    img: "woman_hero_1_mobile.webp",
   },
   {
     title: "Lorem ipsum dolor sit amet consectetur",
+    alt: "a woman stretching her legs",
     text: "Lorem ipsum dolor sit amet consectetur. Magna faucibus sed est facilisis elementum. Mauris enim fringilla diam ultricies suspendisse purus elit.",
-    img: "woman_hero_2.webp",
+    img: "woman_hero_2_mobile.webp",
   },
   {
     title: "Lorem ipsum dolor",
+    alt: "a woman smiling",
     text: "Lorem ipsum dolor sit amet consectetur. Magna faucibus sed est facilisis elementum. Mauris enim fringilla diam.",
-    img: "woman_hero_3.webp",
+    img: "woman_hero_3_mobile.webp",
   },
 ]);
 </script>
