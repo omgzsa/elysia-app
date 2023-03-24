50ca9c415a779b2cf2ee19977222066c69224d6b
omgzsa <zselloandras@gmail.com>
2023-03-24 15:53:41 +0100

    created testimonials

---
+++ frontend/src/components/home/HomeTestimonials.vue
@@ -0,0 +1,13 @@
+<script setup>
+defineProps({
+  title: {
+    type: String,
+  },
+});
+</script>
+<template>
+  <div>
+    <span class="subheading">elégedett ügyfeleink</span>
+    <h2>{{ title }}</h2>
+  </div>
+</template>
