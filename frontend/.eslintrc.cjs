0d3d4f84775cdf0275fa8acbe90313e56556a450
omgzsa <zselloandras@gmail.com>
2023-03-24 11:50:43 +0100

    delete 'cr' problem solved

--- frontend/.eslintrc.cjs
+++ frontend/.eslintrc.cjs
@@ -3,6 +3,9 @@ require("@rushstack/eslint-patch/modern-module-resolution");
 
 module.exports = {
   root: true,
+  rules: {
+    "prettier/prettier": ["error", { endOfLine: "auto" }],
+  },
   extends: [
     "plugin:vue/vue3-essential",
     "eslint:recommended",
