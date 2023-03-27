9a9cfb51e2ebb2dcece15620c6352676007f34dd
omgzsa <zselloandras@gmail.com>
2023-03-27 15:58:47 +0200

    created bloglist/listitem

---
+++ frontend/src/components/blog/BlogList.vue
@@ -0,0 +1,27 @@
+<script setup>
+import { ref } from "vue";
+import BlogListItem from "./BlogListItem.vue";
+
+const blogs = ref([
+  {
+    id: 1,
+    title: "Lorem ipsum dolor sit amet",
+    text: "Lorem ipsum dolor sit amet consectetur. Magna faucibus sed est facilisis elementum. Mauris enim fringilla diam ultricies suspendisse purus elit. Lorem ipsum dolor sit amet consectetur. Magna faucibus sed est facilisis elementum. Mauris enim fringilla diam ultricies suspendisse purus elit.",
+  },
+  {
+    id: 2,
+    title: "Lorem ipsum dolor sit amet consectetur",
+    text: "Lorem ipsum dolor sit amet consectetur. Magna faucibus sed est facilisis elementum. Mauris enim fringilla diam ultricies suspendisse purus elit. Lorem ipsum dolor sit amet consectetur. Magna faucibus sed est facilisis elementum. Mauris enim fringilla diam ultricies suspendisse purus elit.",
+  },
+  {
+    id: 3,
+    title: "Lorem ipsum dolor",
+    text: "Lorem ipsum dolor sit amet consectetur. Magna faucibus sed est facilisis elementum. Mauris enim fringilla diam ultricies suspendisse purus elit. Lorem ipsum dolor sit amet consectetur. Magna faucibus sed est facilisis elementum. Mauris enim fringilla diam ultricies suspendisse purus elit.",
+  },
+]);
+</script>
+<template>
+  <div>
+    <BlogListItem v-for="blog in blogs" :key="blog.id" :blog="blog" />
+  </div>
+</template>
---
+++ frontend/src/components/blog/BlogListItem.vue
@@ -0,0 +1,14 @@
+<script setup>
+defineProps({
+  blog: {
+    type: Object,
+    required: true,
+  },
+});
+</script>
+<template>
+  <div class="flex flex-col justify-center items-center gap-4">
+    <h2 class="text-2xl font-bold text-center">{{ blog.title }}</h2>
+    <p class="text-center">{{ blog.text }}</p>
+  </div>
+</template>
