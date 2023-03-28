0c3ba89f201a01996a33c8546ad9b303d1cb2eeb
omgzsa <zselloandras@gmail.com>
2023-03-28 20:01:39 +0200

    contact component added

---
+++ frontend/src/components/TheContact.vue
@@ -0,0 +1,22 @@
+<script setup>
+import { useGetImageUrl } from "@/composables/getImageUrl.js";
+
+const image = "contact_bg.webp";
+</script>
+
+<template>
+  <section
+    class="flex items-center justify-center h-[600px] m-auto mb-12 bg-fixed bg-center bg-cover"
+    :style="{ backgroundImage: `url(${useGetImageUrl(image)})` }"
+  >
+    <div class="p-5 text-2xl text-white bg-opacity-50 rounded-xl">
+      <!-- Parallax inline -->
+    </div>
+    <div>
+      <div>
+        <h2>Foglalja le időpontját pár kattintással!</h2>
+      </div>
+      <div></div>
+    </div>
+  </section>
+</template>
