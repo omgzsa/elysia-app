28622e3ba26fedffb0062a1d1abe3dcdfdbd3bc5
omgzsa <zselloandras@gmail.com>
2023-03-29 12:37:02 +0200

    added gMaps

--- frontend/src/components/TheContact.vue
+++ frontend/src/components/TheContact.vue
@@ -1,12 +1,17 @@
 <script setup>
+import { ref } from "vue";
 import { useGetImageUrl } from "@/composables/getImageUrl.js";
 import { RouterLink } from "vue-router";
+import { GoogleMap, Marker } from "vue3-google-map";
 
 import IconClock from "../components/icons/IconClock.vue";
 import IconEmail from "../components/icons/IconEmail.vue";
 import IconPhone from "../components/icons/IconPhone.vue";
 import IconMarker from "../components/icons/IconMarker.vue";
 import IconCalendar from "../components/icons/IconCalendar.vue";
+
+const center = ref({ lat: 47.6931539, lng: 17.6366309 });
+const apiKey = import.meta.env.VITE_GOOGLE_MAPS_API_KEY;
 
 const image = "contact_bg.webp";
 const openHours = "Hétfő - Péntek: 8:00 - 18:00";
@@ -26,7 +31,7 @@ defineProps({
   <div>
     <section class="mb-0">
       <div
-        class="flex items-center justify-center h-72 md:h-[600px] m-auto my-6 bg-fixed bg-center bg-[length:960px_349px] bg-no-repeat md:bg-[length:1440px_524px] xl:bg-cover"
+        class="flex items-center justify-center h-72 md:h-[600px] m-auto mt-6 bg-fixed bg-center bg-[length:960px_349px] bg-no-repeat md:bg-[length:1440px_524px] xl:bg-cover"
         :style="{ backgroundImage: `url(${useGetImageUrl(image)})` }"
       >
         <div class="p-5 text-2xl text-white bg-opacity-50 rounded-xl">
@@ -35,9 +40,9 @@ defineProps({
       </div>
     </section>
 
-    <div class="container m-auto grid grid-cols-1 md:grid-cols-2">
+    <div class="m-auto grid grid-cols-1 md:grid-cols-2">
       <!-- contact data -->
-      <div class="space-y-6 p-6 mx-auto md:p-12 lg:p-20 max-w-3xl">
+      <div class="space-y-8 p-6 mx-auto md:p-12 lg:p-20 max-w-3xl">
         <h2>{{ title }}</h2>
 
         <ul class="space-y-2">
@@ -59,7 +64,7 @@ defineProps({
           </li>
         </ul>
 
-        <p class="pb-4">
+        <p class="pb-8">
           Ha nem találja amit keres nézzen körül Gyakran Ismételt Kérdések
           között.
         </p>
@@ -80,7 +85,16 @@ defineProps({
       </div>
 
       <!-- google maps -->
-      <div>GOOGLE MAPS</div>
+      <div>
+        <GoogleMap
+          :api-key="apiKey"
+          style="width: 100%; height: 100%"
+          :center="center"
+          :zoom="15"
+        >
+          <Marker :options="{ position: center }" />
+        </GoogleMap>
+      </div>
     </div>
   </div>
 </template>
