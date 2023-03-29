ea0abfc70f777f92fc21e88b757cafaf0b0844b2
omgzsa <zselloandras@gmail.com>
2023-03-29 20:50:27 +0200

    added getImage with blog images

--- frontend/src/components/blog/BlogListItem.vue
+++ frontend/src/components/blog/BlogListItem.vue
@@ -1,21 +1,27 @@
 <script setup>
+import { useGetImageUrl } from "../../composables/getImageUrl";
+
 defineProps({
   blog: {
     type: Object,
     required: true,
   },
+  // image: {
+  //   type: String,
+  //   required: true,
+  // },
 });
 </script>
 <template>
   <a
     rel="noopener noreferrer"
     href="#"
-    class="max-w-sm mx-auto group hover:no-underline focus:no-underline border border-gray-400 rounded-xl"
+    class="max-w-xs sm:max-w-sm mx-auto group hover:no-underline focus:no-underline border border-gray-400 rounded-xl"
   >
     <img
       role="presentation"
       class="object-cover w-full rounded-t-xl h-44 dark:bg-gray-500"
-      src="https://source.unsplash.com/random/480x360?1"
+      :src="useGetImageUrl(blog.image)"
     />
     <div class="p-6 space-y-2">
       <h3
