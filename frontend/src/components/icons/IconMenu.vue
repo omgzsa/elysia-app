79a8e7941e2fc7e9144b00573d05a4e52c3a14e9
omgzsa <zselloandras@gmail.com>
2023-03-31 13:09:28 +0200

    added menu icons

---
+++ frontend/src/components/icons/IconMenu.vue
@@ -0,0 +1,19 @@
+<template>
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="1.25em"
+    height="1.25em"
+    viewBox="0 0 24 24"
+  >
+    <path
+      fill="currentColor"
+      d="M3 6h18v2H3V6m0 5h18v2H3v-2m0 5h18v2H3v-2Z"
+    ></path>
+  </svg>
+</template>
+
+<script>
+export default {
+  name: "IconMenu",
+};
+</script>
---
+++ frontend/src/components/icons/IconMenuShort.vue
@@ -0,0 +1,16 @@
+<template>
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="1.25em"
+    height="1.25em"
+    viewBox="0 0 24 24"
+  >
+    <path fill="currentColor" d="M4 9h16v2H4V9m0 4h10v2H4v-2Z"></path>
+  </svg>
+</template>
+
+<script>
+export default {
+  name: "IconMenuShort",
+};
+</script>
