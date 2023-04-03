5b4a417f093b3192f0339ea9ee5eb7a91d725b47
omgzsa <zselloandras@gmail.com>
2023-04-03 10:17:27 +0200

    added services array for dropdown menu

--- frontend/src/components/TheNavbar.vue
+++ frontend/src/components/TheNavbar.vue
@@ -1,104 +1,327 @@
 <script setup>
-import { ref } from "vue";
+import { ref, onMounted, onUnmounted } from "vue";
 import { RouterLink } from "vue-router";
 import { useGetImageUrl } from "../composables/getImageUrl";
+import { onClickOutside } from "@vueuse/core";
+
 import TheNavbarMobile from "./TheNavbarMobile.vue";
 
 // icons
 import IconMenu from "./icons/IconMenu.vue";
-// import IconMenuShort from "./icons/IconMenuShort.vue";
 import IconCalendar from "./icons/IconCalendar.vue";
 import IconWindowClose from "./icons/IconWindowClose.vue";
+import TheDropdown from "./TheDropdown.vue";
+// import AppLink from "./AppLink.vue";
 
-const isMobileMenuOpen = ref(false);
-const toggleNav = () => (isMobileMenuOpen.value = !isMobileMenuOpen.value);
-
+// props
 defineProps({
   logo: {
     type: String,
     required: true,
   },
 });
+
+const services = ref([
+  {
+    title: "Bőrgyógyászat",
+    link: "/szolgaltatasok/borgyogyaszat",
+    children: [
+      {
+        title: "Gyermek bőrgyógyászat",
+        link: "/szolgaltatasok/borgyogyaszat/gyermek-borgyogyaszat",
+      },
+      {
+        title: "Dermatoscopos anyajegy- és bőrrák szűrés",
+        link: "/szolgaltatasok/borgyogyaszat/dermatoscopos-anyajegy-es-borrak-szures",
+      },
+      {
+        title: "Pattanásos bőr komplex kezelése",
+        link: "/szolgaltatasok/borgyogyaszat/pattanasos-bor-komplex-kezelese",
+      },
+      {
+        title: "Bőr- és körömgombásodás kezelése",
+        link: "/szolgaltatasok/borgyogyaszat/bor-es-koromgombasodas-kezeles",
+      },
+      {
+        title: "Ekcéma, atópia, allergiás bőrbetegségek kezelése",
+        link: "/szolgaltatasok/borgyogyaszat/ekcema-atopia-allergias-borbetegsegek-kezelese",
+      },
+      {
+        title: "Pikkelysömör (Pszoriázis) kezelés",
+        link: "/szolgaltatasok/borgyogyaszat/pikkelysomor-kezeles",
+      },
+      {
+        title: "Vírusos szemölcsök kezelése",
+        link: "/szolgaltatasok/borgyogyaszat/virusos-szemolcsok-kezelese",
+      },
+      {
+        title: "Hajhullás kivizsgálása és kezelése",
+        link: "/szolgaltatasok/borgyogyaszat/hujhullas-kivizsgalas-es-kezeles",
+      },
+    ],
+  },
+  {
+    title: "Tüdőgyógyászati Szakrendelés",
+  },
+  {
+    title: "Idegsebészeti vizsgálatok",
+  },
+  {
+    title: "Lézerkezelések",
+    children: [
+      {
+        title: "Lézeres arcfiatalítás",
+        link: "/szolgaltatasok/lezerkezelesek/lezeres-arcfiatalitas",
+      },
+      {
+        title: "Vagina rejuvenáció (hüvelyszűkítés)",
+        link: "/szolgaltatasok/lezerkezelesek/vagina-rejuvenacio",
+      },
+      {
+        title: "Lézeres szőrtelenítés",
+        link: "/szolgaltatasok/lezerkezelesek/lezeres-szortelenites",
+      },
+      {
+        title: "Seprűvénák, értágulatok kezelése",
+        link: "/szolgaltatasok/lezerkezelesek/sepruvenak-ertagulatok-kezelese",
+      },
+      {
+        title: "Pigmentfoltok kezelése",
+        link: "/szolgaltatasok/lezerkezelesek/pigmentfolt-kezeles",
+      },
+      {
+        title: "Akne kezelés",
+        link: "/szolgaltatasok/lezerkezelesek/akne-kezeles",
+      },
+      {
+        title: "Stria kezelés",
+        link: "/szolgaltatasok/lezerkezelesek/stria-kezeles",
+      },
+      {
+        title: "Körömgomba kezelése",
+        link: "/szolgaltatasok/lezerkezelesek/koromgomba-kezeles",
+      },
+    ],
+  },
+  {
+    title: "Orvosi esztétika",
+    children: [
+      {
+        title: "Profhiló kezelés",
+        link: "/szolgaltatasok/orvos-esztetika/profhilo-kezeles",
+      },
+      {
+        title: "Arc és nyak mezoterápiás kezelése",
+        link: "/szolgaltatasok/orvos-esztetika/arc-es-nyak-mezoterapias-kezelese",
+      },
+      {
+        title: "Hajas fejbőr mezoterápiás kezelése",
+        link: "/szolgaltatasok/orvos-esztetika/hajas-fejbor-mezoterapias-kezelese",
+      },
+      {
+        title: "Dermaroller kezelés",
+        link: "/szolgaltatasok/orvos-esztetika/demaroller-kezeles",
+      },
+      {
+        title: "Fokozott hónalji izzadás kezelése",
+        link: "/szolgaltatasok/orvos-esztetika/fokozott-honalji-izzadas-kezeles",
+      },
+      {
+        title: "Ráncfeltöltés hyaluronsavval",
+        link: "/szolgaltatasok/orvos-esztetika/rancfeltoltes-hyaluronsavval",
+      },
+      {
+        title: "Ránctalanítás botulinum toxin injekcióval",
+        link: "/szolgaltatasok/orvos-esztetika/ranc-talanitas-botulinum-toxin-injekcioval",
+      },
+      {
+        title: "Thread lifting: bioszálas arckontúrozás",
+        link: "/szolgaltatasok/orvos-esztetika/thread-lifting-bioszalas-arckonturozas",
+      },
+      {
+        title: "PRP/Drakula terápia",
+        link: "/szolgaltatasok/orvos-esztetika/prp-drakula-terapia",
+      },
+      {
+        title: "Skin booster kezelés",
+        link: "/szolgaltatasok/orvos-esztetika/skin-booster-kezeles",
+      },
+    ],
+  },
+  {
+    title: "Plasztikai sebészet",
+    children: [
+      {
+        title: "Anyajegy eltávolítása szövettani vizsgálattal",
+        link: "/szolgaltatasok/plasztikai-sebeszet/anyajegy-eltavolitasa-szovettani-vizsgalattal",
+      },
+      {
+        title: "Alsó, felső szemhéjplasztika",
+        link: "/szolgaltatasok/plasztikai-sebeszet/also-felso-szemhejplasztika",
+      },
+      {
+        title: "Fülkorrekció",
+        link: "/szolgaltatasok/plasztikai-sebeszet/fulkorrekcio",
+      },
+      {
+        title: "Fülllyuk korrekció",
+        link: "/szolgaltatasok/plasztikai-sebeszet/fulllyuk-korrekcio",
+      },
+      {
+        title: "Kisajak plasztika",
+        link: "/szolgaltatasok/plasztikai-sebeszet/kisajak-plasztika",
+      },
+      {
+        title: "Orrkorrekció",
+        link: "/szolgaltatasok/plasztikai-sebeszet/orrkorrekcio",
+      },
+      {
+        title: "Hegkorrekció",
+        link: "/szolgaltatasok/plasztikai-sebeszet/hegkorrekcio",
+      },
+      {
+        title: "Ajakfeltöltés",
+        link: "/szolgaltatasok/plasztikai-sebeszet/ajakfeltoltes",
+      },
+      {
+        title: "COG szálas arclifting",
+        link: "/szolgaltatasok/plasztikai-sebeszet/cog-szalas-arclifting",
+      },
+    ],
+  },
+  {
+    title: "Diagnosztikai vizsgálatok",
+    children: [
+      {
+        title: "Standard epicutan teszt",
+        link: "/szolgaltatasok/diagnosztikai-vizsgalatok/standard-epicutan-teszt",
+      },
+      {
+        title: "Laboratóriumi vizsgálatok",
+        link: "/szolgaltatasok/diagnosztikai-vizsgalatok/laboratoriumi-vizsgalatok",
+      },
+    ],
+  },
+  {
+    title: "Orvosi termékek",
+    children: [
+      {
+        title: "Z0 Skin Health",
+        link: "/szolgaltatasok/orvosi-termek/z0-skin-health",
+      },
+      {
+        title: "La Roche Posay",
+        link: "/szolgaltatasok/orvosi-termek/la-roche-posay",
+      },
+      {
+        title: "CeraVe",
+        link: "/szolgaltatasok/orvosi-termek/cerave",
+      },
+      {
+        title: "Vichy",
+        link: "/szolgaltatasok/orvosi-termek/vichy",
+      },
+      {
+        title: "Bioderma",
+        link: "/szolgaltatasok/orvosi-termek/bioderma",
+      },
+      {
+        title: "Thalgo",
+        link: "/szolgaltatasok/orvosi-termek/thalgo",
+      },
+      {
+        title: "Orvosi lézerkészülékek",
+        link: "/szolgaltatasok/orvosi-termek/orvosi-lezerkeszulekek",
+      },
+    ],
+  },
+]);
+
+// variables
+const target = ref(null);
+const showNavbar = ref(true);
+const lastScrollPosition = ref(0);
+
+onClickOutside(target, () => {
+  isMobileMenuOpen.value = false;
+});
+
+const isMobileMenuOpen = ref(false);
+const toggleNav = () => (isMobileMenuOpen.value = !isMobileMenuOpen.value);
+
+// scrollPosition check
+function onScroll() {
+  const currentScrollPosition =
+    window.scrollY || document.documentElement.scrollTop;
+
+  if (currentScrollPosition <= 60) {
+    document.querySelector(".navbar").style.backgroundColor = "transparent";
+  } else {
+    document.querySelector(".navbar").style.background =
+      "rgba(255, 255, 255, 1)";
+  }
+
+  if (currentScrollPosition < 0) {
+    return;
+  }
+  if (Math.abs(currentScrollPosition - lastScrollPosition.value) < 60) {
+    return;
+  }
+  showNavbar.value = currentScrollPosition < lastScrollPosition.value;
+  lastScrollPosition.value = currentScrollPosition;
+}
+
+onMounted(() => {
+  window.addEventListener("scroll", onScroll);
+});
+
+onUnmounted(() => {
+  window.removeEventListener("scroll", onScroll);
+});
 </script>
 
 <template>
-  <div class="relative mx-auto max-w-screen-xl px-2 sm:px-4 lg:px-6">
-    <div class="flex h-16 items-center justify-between mt-1">
+  <nav
+    class="navbar mx-auto max-w-screen-xl pl-1 pr-3 md:pr-2 py-1"
+    :class="{ 'navbar--hidden': !showNavbar }"
+    aria-label="Site Nav"
+  >
+    <div
+      class="relative flex px-2 sm:px-4 lg:px-6 h-12 sm:h-16 items-center justify-between"
+    >
       <!-- LOGO -->
       <div class="md:flex md:items-center md:gap-12">
-        <a class="block text-teal-600 w-12 sm:w-16 md:w-20" href="/">
+        <AppLink :to="{ name: 'home' }" class="block w-12 sm:w-16" href="/">
           <span class="sr-only">Home</span>
           <img :src="useGetImageUrl(logo)" alt="" />
-        </a>
+        </AppLink>
       </div>
 
       <!-- DESKTOP MENU -->
-      <div class="hidden lg:block">
-        <nav aria-label="Site Nav">
-          <ul
-            class="flex items-center gap-6 font-bold text-xs lg:text-sm xl:text-base"
-          >
-            <li>
-              <a
-                class="text-gray-500 transition hover:text-secondary-100 active:hover:text-secondary-100"
-                href="/"
-              >
-                Kezdőlap
-              </a>
-            </li>
+      <div class="hidden lg:block ml-auto">
+        <ul
+          class="flex items-center gap-6 font-medium text-xs lg:text-sm xl:text-base"
+        >
+          <AppLink :to="{ name: 'home' }"> Kezdőlap </AppLink>
 
-            <li>
-              <a
-                class="text-gray-500 transition hover:text-secondary-100 active:hover:text-secondary-100"
-                href="/"
-              >
-                Szolgáltatások
-              </a>
-            </li>
+          <TheDropdown title="Szolgáltatások" :services="services">
+            Szolgáltatások
+          </TheDropdown>
 
-            <li>
-              <a
-                class="text-gray-500 transition hover:text-secondary-100 active:hover:text-secondary-100"
-                href="/"
-              >
-                Z0 Skin Health
-              </a>
-            </li>
+          <AppLink :to="{ name: 'home' }"> Z0 Skin Health </AppLink>
 
-            <li>
-              <a
-                class="text-gray-500 transition hover:text-secondary-100 active:hover:text-secondary-100"
-                href="/"
-              >
-                Áraink
-              </a>
-            </li>
+          <AppLink :to="{ name: 'home' }"> Áraink </AppLink>
 
-            <li>
-              <a
-                class="text-gray-500 transition hover:text-secondary-100 active:hover:text-secondary-100"
-                href="/"
-              >
-                Munkatársak
-              </a>
-            </li>
+          <AppLink :to="{ name: 'home' }"> Munkatársak </AppLink>
 
-            <li>
-              <a
-                class="text-gray-500 transition hover:text-secondary-100 active:hover:text-secondary-100"
-                href="/"
-              >
-                Kapcsolat
-              </a>
-            </li>
-          </ul>
-        </nav>
+          <AppLink :to="{ name: 'home' }"> Kapcsolat </AppLink>
+        </ul>
       </div>
 
       <!-- IDŐPONTFOGLALÁS -->
-      <div class="flex items-center gap-2">
+      <div class="flex items-center gap-2 ml-auto mr-1">
         <div class="sm:flex sm:gap-4">
-          <RouterLink :to="{ name: 'services', path: '/services' }">
+          <RouterLink :to="{ name: 'home', path: '/' }">
             <button
               class="rounded-md px-4 py-2 overflow-hidden relative group cursor-pointer font-medium text-gray-700 bg-secondary-100 border border-secondary-100"
             >
@@ -112,57 +335,41 @@ defineProps({
             </button>
           </RouterLink>
         </div>
+      </div>
 
-        <!-- MOBILE MENU button -->
-        <div class="block lg:hidden">
-          <button
-            @click="toggleNav"
-            :aria-expanded="isMobileMenuOpen ? 'true' : 'false'"
-            aria-label="Toggle mobile menu"
-            class="rounded p-1.5 sm:p-2 text-gray-500 transition hover:text-gray-700 hover:bg-gray-200"
-          >
-            <IconMenu :class="{ hidden: isMobileMenuOpen }" />
-            <IconWindowClose :class="{ hidden: !isMobileMenuOpen }" />
-          </button>
-        </div>
+      <!-- MOBILE MENU button -->
+      <div class="block lg:hidden">
+        <button
+          @click="toggleNav"
+          :aria-expanded="isMobileMenuOpen ? 'true' : 'false'"
+          aria-label="Toggle mobile menu"
+          class="rounded p-1.5 sm:p-2 text-gray-500 transition hover:text-gray-700 hover:bg-gray-200"
+        >
+          <IconMenu :class="{ hidden: isMobileMenuOpen }" />
+          <IconWindowClose :class="{ hidden: !isMobileMenuOpen }" />
+        </button>
       </div>
     </div>
 
     <!-- Mobile sidebar -->
-    <!-- <Transition name="slide"> -->
     <div :class="{ block: isMobileMenuOpen, hidden: !isMobileMenuOpen }">
-      <TheNavbarMobile />
+      <TheNavbarMobile :services="services" ref="target" />
     </div>
-    <!-- </Transition> -->
-  </div>
+  </nav>
 </template>
 
 <style>
-/* slide in animation */
-/* .slide-enter-active {
-  animation: slide-in 0.3s ease-out forwards;
+.navbar {
+  width: 100vw;
+  background: #ffffff20;
+  position: fixed;
+  transform: translate3d(0, 0, 0);
+  transition: 0.15s all ease-out;
+  z-index: 1;
 }
 
-@keyframes slide-in {
-  from {
-    transform: translateX(-100%);
-  }
-  to {
-    transform: translateX(0);
-  }
-} */
-
-/* slide out animation */
-/* .slide-leave-active {
-  animation: slide-out 0.3s ease-out forwards;
+.navbar.navbar--hidden {
+  box-shadow: none;
+  transform: translate3d(0, -100%, 0);
 }
-
-@keyframes slide-out {
-  from {
-    transform: translateX(0);
-  }
-  to {
-    transform: translateX(-100%);
-  }
-} */
 </style>
