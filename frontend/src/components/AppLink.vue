599b1418be60e2a667f6f8ca09ef3ea476145d16
omgzsa <zselloandras@gmail.com>
2023-04-03 10:16:08 +0200

    Applink with external link

---
+++ frontend/src/components/AppLink.vue
@@ -0,0 +1,30 @@
+<script setup>
+import { RouterLink } from "vue-router";
+import { computed } from "vue";
+
+const props = defineProps({
+  ...RouterLink.props,
+});
+
+const isExternal = computed(
+  () => typeof props.to === "string" && props.to.startsWith("http")
+);
+</script>
+
+<template>
+  <template v-if="isExternal">
+    <a
+      class="cursor-pointer hover:text-secondary-100 transition"
+      :href="to"
+      rel="noopener"
+      target="_blank"
+      ><slot
+    /></a>
+  </template>
+  <RouterLink
+    class="cursor-pointer hover:text-secondary-100 transition"
+    v-else
+    v-bind="$props"
+    ><slot
+  /></RouterLink>
+</template>
