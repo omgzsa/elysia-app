a81d41d5beace16874760a15f2c1b0a440a4c400
omgzsa <zselloandras@gmail.com>
2023-04-04 11:33:06 +0200

    optimizing dropdown

--- frontend/src/components/TheDropdown.vue
+++ frontend/src/components/TheDropdown.vue
@@ -1,20 +1,23 @@
 <script setup>
 import { ref } from "vue";
 import IconDown from "./icons/IconDown.vue";
+import IconRight from "./icons/IconRight.vue";
+// import { onClickOutside } from "@vueuse/core";
+//
 
 defineProps({
-  title: {
-    type: String,
-    required: true,
-    default: "menü",
-  },
-  services: {
+  submenu: {
     type: Array,
     required: true,
   },
 });
 
 const isVisible = ref(false);
+// const modal = ref(null);
+
+// onClickOutside(modal, () => {
+//   isVisible.value = false;
+// });
 
 const toggleVisibility = () => {
   isVisible.value = !isVisible.value;
@@ -22,22 +25,28 @@ const toggleVisibility = () => {
 </script>
 
 <template>
-  <div @click="toggleVisibility" class="relative cursor-pointer">
-    <div class="flex items-center space-x-2 hover:text-secondary-100">
-      <p class="text-sm">{{ title }}</p>
-      <IconDown />
+  <div class="relative cursor-pointer">
+    <div
+      class="flex items-center space-x-2 hover:text-secondary-100"
+      @click="toggleVisibility"
+    >
+      <p class="text-sm"><slot /></p>
+      <IconDown v-if="isVisible" />
+      <IconRight v-else />
     </div>
     <Transition name="dropdown-fade">
       <ul
         v-if="isVisible"
-        class="absolute right-6 sm:right-36 lg:right-auto top-3 max-h-max w-max p-6 border rounded-xl bg-white flex flex-col space-y-1"
+        class="absolute z-20 inset-0 sm:right-36 lg:right-auto top-4 xl:top-6 max-h-max w-60 sm:w-max p-6 border rounded-xl bg-white flex flex-col space-y-1"
       >
         <li
-          v-for="item in services"
+          v-for="item in submenu"
           :key="item.title"
-          class="hover:text-secondary-100"
+          class="hover:text-secondary-100 flex space-x-1"
         >
-          <p class="text-sm">{{ item.title }}</p>
+          <p class="text-sm">
+            {{ item.title }}
+          </p>
         </li>
       </ul>
     </Transition>
