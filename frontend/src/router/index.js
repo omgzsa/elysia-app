7d3e3e8f2096d47b1488260527b876454bb5379c
omgzsa <zselloandras@gmail.com>
2023-04-06 17:23:42 +0200

    dynamic route to service categories

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -15,49 +15,10 @@ const router = createRouter({
       component: () => import("../views/AboutView.vue"),
     },
     {
-      path: "/szolgaltatasok",
-      name: "services",
-      component: () => import("../views/ServicesView.vue"),
-    },
-    {
-      path: "/szolgaltatasok/borgyogyaszat",
-      name: "services.borgyogyaszat",
-      component: () => import("../views/services/Borgyogyaszat.vue"),
-    },
-    {
-      path: "/szolgaltatasok/tudogyogyaszati-szakrendeles",
-      name: "services.tudogyogyaszat",
-      component: () => import("../views/services/Tudogyogyaszat.vue"),
-    },
-    {
-      path: "/szolgaltatasok/idegsebeszeti-vizsgalatok",
-      name: "services.idegsebeszet",
-      component: () => import("../views/services/Idegsebeszet.vue"),
-    },
-    {
-      path: "/szolgaltatasok/lezerkezelesek",
-      name: "services.lezerkezeles",
-      component: () => import("../views/services/Lezerkezeles.vue"),
-    },
-    {
-      path: "/szolgaltatasok/orvos-esztetika",
-      name: "services.esztetika",
-      component: () => import("../views/services/OrvosEsztetika.vue"),
-    },
-    {
-      path: "/szolgaltatasok/plasztikai-sebeszet",
-      name: "services.plasztika",
-      component: () => import("../views/services/Plasztika.vue"),
-    },
-    {
-      path: "/szolgaltatasok/diagnosztikai-vizsgalatok",
-      name: "services.diagnosztika",
-      component: () => import("../views/services/Diagnosztika.vue"),
-    },
-    {
-      path: "/szolgaltatasok/orvosi-termekek",
-      name: "services.termekek",
-      component: () => import("../views/services/Termekek.vue"),
+      path: "/szolgaltatas/:slug",
+      name: "services.category",
+      component: () => import("../views/ServicesCategoryView.vue"),
+      props: true,
     },
   ],
 });
