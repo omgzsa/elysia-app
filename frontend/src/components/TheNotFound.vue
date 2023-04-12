3f6fca9a4ee86f417e016ec50e922cfed8002b9c
omgzsa <zselloandras@gmail.com>
2023-04-12 16:28:35 +0200

    notFound page lil styles

---
+++ frontend/src/components/TheNotFound.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="grid min-h-screen px-4 bg-white place-content-center gap-4">
+    <h1 class="tracking-widest font-normal text-gray-500 uppercase">
+      404 | Not Found
+    </h1>
+    <div>
+      <p class="text-gray-500">
+        Térj vissza a
+        <AppLink class="underline underline-offset-4" :to="{ name: 'home' }"
+          >kezdőlapra</AppLink
+        >
+      </p>
+    </div>
+  </div>
+</template>
