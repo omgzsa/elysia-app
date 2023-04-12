cfad1f657b64ea91f22e8c22d7c8058cc4aef341
omgzsa <zselloandras@gmail.com>
2023-04-12 18:19:10 +0200

    arrow icon for category template

---
+++ frontend/src/components/icons/IconArrowRight.vue
@@ -0,0 +1,23 @@
+<template>
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="1.25em"
+    height="1.25em"
+    viewBox="0 0 24 24"
+  >
+    <path
+      fill="none"
+      stroke="currentColor"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      strokeWidth="1.5"
+      d="M4 12h16m0 0l-6-6m6 6l-6 6"
+    ></path>
+  </svg>
+</template>
+
+<script>
+export default {
+  name: "IconArrowRight",
+};
+</script>
