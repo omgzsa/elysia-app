89b26781b59772a022d67d4fea259491e25c3a50
omgzsa <zselloandras@gmail.com>
2023-04-12 16:29:47 +0200

    beforeEnter route guard to :category page

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from "vue-router";
 import HomeView from "../views/HomeView.vue";
+import sourceData from "@/assets/services.json";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -24,6 +25,20 @@ const router = createRouter({
       name: "services.category",
       component: () => import("../views/ServicesCategoryView.vue"),
       props: true,
+      beforeEnter(to) {
+        const exists = sourceData.find(
+          (item) => item.slug === to.params.category
+        );
+        if (!exists) {
+          return {
+            name: "not.found",
+            // allows keeping the URL in the browser address bar while rendering the 404 page
+            params: { pathMatch: to.path.substring(1).split("/") },
+            query: to.query,
+            hash: to.hash,
+          };
+        }
+      },
     },
     {
       path: "/szolgaltatas/:category/:slug",
@@ -31,7 +46,19 @@ const router = createRouter({
       component: () => import("../views/ServiceSingleView.vue"),
       props: true,
     },
+    {
+      path: "/:pathMatch(.*)*",
+      name: "not.found",
+      component: () => import("../components/TheNotFound.vue"),
+    },
   ],
+  scrollBehavior(to, from, savedPosition) {
+    if (savedPosition) {
+      return savedPosition;
+    } else {
+      return { top: 0, behavior: "smooth" };
+    }
+  },
 });
 
 export default router;
