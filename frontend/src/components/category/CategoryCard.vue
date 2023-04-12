547da39c89a9b8e0ed8c1cfe95606e6d32b7a25c
omgzsa <zselloandras@gmail.com>
2023-04-12 18:19:30 +0200

    added image and arrow icon

--- frontend/src/components/category/CategoryCard.vue
+++ frontend/src/components/category/CategoryCard.vue
@@ -1,4 +1,8 @@
 <script setup>
+import { useGetServiceImageUrl } from "../../composables/getServiceImageUrl";
+
+import IconArrowRight from "@/components/icons/IconArrowRight.vue";
+
 defineProps({
   item: {
     type: Object,
@@ -9,9 +13,15 @@ defineProps({
 
 <template>
   <div class="category-card">
-    <img src="" alt="" />
+    <img
+      :src="useGetServiceImageUrl(item.image)"
+      :alt="item.name"
+      class="rounded-xl w-full h-52 object-cover object-top"
+    />
     <h3>{{ item.name }}</h3>
-    <button>Bővebben</button>
+    <button class="text-xs text-gray-500 flex gap-4 items-center">
+      Bővebben <IconArrowRight />
+    </button>
     <!-- <AppLink /> -->
   </div>
 </template>
