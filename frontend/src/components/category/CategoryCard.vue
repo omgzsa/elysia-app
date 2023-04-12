ff3588c4a6f4ba70d7aba62e5aafc806fef899fd
omgzsa <zselloandras@gmail.com>
2023-04-12 16:28:18 +0200

    fleshing out category cards

---
+++ frontend/src/components/category/CategoryCard.vue
@@ -0,0 +1,17 @@
+<script setup>
+defineProps({
+  item: {
+    type: Object,
+    required: true,
+  },
+});
+</script>
+
+<template>
+  <div class="category-card">
+    <img src="" alt="" />
+    <h3>{{ item.name }}</h3>
+    <button>Bővebben</button>
+    <!-- <AppLink /> -->
+  </div>
+</template>
