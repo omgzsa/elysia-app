deb684eec6cb45753d6368e9d3662b02cd8c20d7
omgzsa <zselloandras@gmail.com>
2023-04-13 15:15:20 +0200

    added route to single treatment

--- frontend/src/components/category/CategoryCard.vue
+++ frontend/src/components/category/CategoryCard.vue
@@ -12,16 +12,25 @@ defineProps({
 </script>
 
 <template>
-  <div class="category-card">
-    <img
-      :src="useGetServiceImageUrl(item.image)"
-      :alt="item.name"
-      class="rounded-xl w-full h-52 object-cover object-top"
-    />
-    <h3>{{ item.name }}</h3>
-    <button class="text-xs text-gray-500 flex gap-4 items-center">
-      Bővebben <IconArrowRight />
-    </button>
-    <!-- <AppLink /> -->
+  <div class="category-card relative">
+    <div
+      class="bg-primary-100/20 h-52 w-full absolute -bottom-5 rounded-xl -z-10"
+    ></div>
+    <div class="px-4 space-y-2">
+      <img
+        :src="useGetServiceImageUrl(item.image)"
+        :alt="item.name"
+        class="rounded-xl w-full h-52 object-cover object-top mb-6"
+      />
+      <h3>{{ item.name }}</h3>
+
+      <AppLink
+        tag="button"
+        :to="{ name: 'service.single', params: { slug: item.slug } }"
+        class="text-xs sm:text-sm text-gray-500 flex gap-4 items-center"
+      >
+        Bővebben <IconArrowRight />
+      </AppLink>
+    </div>
   </div>
 </template>
