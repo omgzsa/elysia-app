d5e7cd245868bcd0b6b1a033b4cf0e2954867f73
omgzsa <zselloandras@gmail.com>
2023-04-14 16:34:58 +0200

    refactored animation to NOT show scrollbar

--- frontend/src/components/home/HomeIntroduction.vue
+++ frontend/src/components/home/HomeIntroduction.vue
@@ -11,31 +11,31 @@ defineProps({
     <div
       class="md:order-1 grid grid-cols-1 gap-y-6 mx-auto text-center md:text-left lg:text-left px-2 max-w-3xl"
     >
-      <span class="subheading" v-motion-slide-visible-once-right :delay="500"
-        >bemutatkozás</span
-      >
-      <h2 v-motion-slide-visible-once-right :delay="350">{{ title }}</h2>
-      <p class="mb-8 sm:mb-12" v-motion-slide-visible-once-top :delay="700">
+      <span class="subheading">bemutatkozás</span>
+      <h2>{{ title }}</h2>
+      <p class="mb-8 sm:mb-12">
         <slot name="introduction" />
       </p>
     </div>
 
-    <div
-      class="grid grid-cols-12 grid-rows-5 p-4 gap-4 items-center"
-      v-motion-slide-visible-once-left
-      :delay="350"
-    >
+    <div class="grid grid-cols-12 grid-rows-5 p-4 gap-4 items-center">
       <img
+        v-motion-slide-visible-once-bottom
+        :delay="900"
         class="row-start-2 row-span-3 col-start-2 col-span-4"
         src="../../assets/images/elysia_intro_2.webp"
         alt="szájfeltöltés fecskendővel"
       />
       <img
+        v-motion-slide-visible-once-bottom
+        :delay="500"
         class="row-start-1 row-span-3 col-start-6 col-span-6 mb-12 sm:mb-16 md:mb-12 lg:mb-14"
         src="../../assets/images/elysia_intro_1.webp"
         alt="narancsbőr kezelése"
       />
       <img
+        v-motion-slide-visible-once-bottom
+        :delay="100"
         class="row-start-3 row-span-3 col-start-6 col-span-7 w-full"
         src="../../assets/images/elysia_intro_3.webp"
         alt="arckezelés"
