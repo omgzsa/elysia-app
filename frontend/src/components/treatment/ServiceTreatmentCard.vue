ee9c14b296dab815754a8d324fea65167be08d55
omgzsa <zselloandras@gmail.com>
2023-04-14 18:16:03 +0200

    adjustments / list markers added

--- frontend/src/components/treatment/ServiceTreatmentCard.vue
+++ frontend/src/components/treatment/ServiceTreatmentCard.vue
@@ -21,7 +21,7 @@ const props = defineProps({
       class="grid grid-cols-1 sm:grid-cols-3 space-y-6 sm:space-y-0 gap-y-6 sm:py-8"
     >
       <!-- treatment information -->
-      <div class="site-padding order-2 sm:col-span-2 space-y-4">
+      <div class="site-padding order-2 sm:col-span-2 space-y-6">
         <h3>{{ treatment.content.title }}</h3>
         <p class="whitespace-pre-wrap">
           {{ treatment.content.description }}
@@ -31,7 +31,10 @@ const props = defineProps({
         <h3>{{ treatment.content.title3 }}</h3>
         <p>{{ treatment.content.description3 }}</p>
         <h3>{{ treatment.content.benefitsTitle }}</h3>
-        <ul>
+        <ul
+          role="list"
+          class="marker:text-accent-100 list-disc list list-inside"
+        >
           <li v-for="item in treatment.content.benefits" :key="item.id">
             {{ item.text }}
           </li>
@@ -43,26 +46,36 @@ const props = defineProps({
         v-if="hasData"
       >
         <div
-          class="flex flex-col space-y-6 mx-auto w-64 sm:w-full lg:max-w-md bg-primary-200 py-3 px-3 rounded-xl lg:py-6 lg:px-10 sm:sticky top-20"
+          class="flex flex-col space-y-6 mx-auto w-64 sm:w-full lg:max-w-md bg-primary-200 py-4 px-4 rounded-xl lg:py-6 lg:px-6 xl:py-8 xl:px-10 sm:sticky top-20"
         >
-          <div class="flex items-start space-x-2">
+          <div
+            class="flex items-center space-x-2 sm:flex-col sm:items-start sm:space-x-0 sm:space-y-2 md:flex-row md:items-center md:space-x-2 md:space-y-0"
+          >
             <IconClock
               class="text-accent-100 flex-shrink text-xs lg:text-base"
             />
             <div class="flex flex-col flex-1">
-              <span class="subheading">a kezelés időtartama</span>
+              <span
+                class="text-xs lg:text-sm xl:text-base tracking-wider uppercase text-gray-800"
+                >a kezelés időtartama</span
+              >
               <span
                 class="text-accent-100 font-medium md:font-semibold text-sm"
                 >{{ props.treatment.treatTime }}</span
               >
             </div>
           </div>
-          <div class="flex items-start space-x-2">
+          <div
+            class="flex items-center space-x-2 sm:flex-col sm:items-start sm:space-x-0 sm:space-y-2 md:flex-row md:items-center md:space-x-2 md:space-y-0"
+          >
             <IconLoop
               class="text-accent-100 flex-shrink text-xs lg:text-base"
             />
             <div class="flex flex-col flex-1">
-              <span class="subheading">gyakorisága</span>
+              <span
+                class="text-xs lg:text-sm xl:text-base tracking-wider uppercase text-gray-800"
+                >gyakorisága</span
+              >
               <span
                 class="text-accent-100 font-medium md:font-semibold text-sm"
                 >{{ props.treatment.frequency }}</span
