f7c4808e0395fb11dac44385d624e7fd92dfe29b
omgzsa <zselloandras@gmail.com>
2023-04-15 18:16:51 +0200

    parallax issue on safari

--- frontend/src/components/TheParallax.vue
+++ frontend/src/components/TheParallax.vue
@@ -7,7 +7,7 @@ const image = "contact_bg.webp";
 <template>
   <section>
     <div
-      class="flex items-center justify-center h-72 md:h-[600px] m-auto mt-6 bg-fixed bg-center bg-[length:960px_349px] bg-no-repeat md:bg-[length:1440px_524px] xl:bg-[length:1920px_698px]"
+      class="flex items-center justify-center h-72 md:h-[600px] m-auto mt-6 bg-scroll lg:bg-fixed bg-center bg-[length:960px_349px] bg-no-repeat md:bg-[length:1440px_524px] xl:bg-[length:1920px_698px]"
       :style="{ backgroundImage: `url(${useGetImageUrl(image)})` }"
     ></div>
   </section>
