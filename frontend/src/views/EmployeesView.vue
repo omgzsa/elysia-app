c630a9c0d3d0116e73c38f5dec1e5667dd8582e0
omgzsa <zselloandras@gmail.com>
2023-04-16 20:48:13 +0200

    created employees view

---
+++ frontend/src/views/EmployeesView.vue
@@ -0,0 +1,17 @@
+<script setup>
+import { ref } from "vue";
+import employeeData from "../assets/coworkers.json";
+
+const employees = ref(employeeData);
+</script>
+
+<template>
+  <div>
+    <h1>Employees</h1>
+    <ul>
+      <li v-for="employee in employees" :key="employee.id">
+        {{ employee.name }}
+      </li>
+    </ul>
+  </div>
+</template>
