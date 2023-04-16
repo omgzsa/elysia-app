2ad8891b5b2073b8a7e5f5be9ca94ca564f9c43f
omgzsa <zselloandras@gmail.com>
2023-04-16 20:40:38 +0200

    breadcrumbs v1

--- frontend/src/components/TheBreadCrumbs.vue
+++ frontend/src/components/TheBreadCrumbs.vue
@@ -2,35 +2,48 @@
 import { inject } from "vue";
 
 const treatment = inject("treatment");
+
+// const treatmentName = computed(() => {
+//   if (treatment.value.name.length > 15) {
+//     return treatment.value.name.slice(0, 15) + "...";
+//   } else {
+//     return treatment.value.name;
+//   }
+// });
 </script>
 
 <template>
   <nav aria-label="breadcrumb" class="w-full p-4 site-padding">
-    <ol class="flex h-8 space-x-2">
-      <!-- <li class="flex items-center text-sm text-accent-100/70">
+    <ol class="flex items-center space-x-2">
+      <li
+        class="flex items-center text-xs md:text-sm space-x-2 text-accent-100/70"
+      >
+        <span class="text-gray-400">#</span>
         <AppLink
           rel="noopener noreferrer"
           :to="{ name: 'home' }"
           title="Vissza a kezdőlapra"
-          class="flex items-center hover:underline"
+          class="flex items-center hover:underline pr-1"
           >Kezdőlap</AppLink
         >
-      </li> -->
-      <li class="flex items-center space-x-1 text-sm text-accent-100/70">
-        <span class="text-gray-400">#</span>
+      </li>
+      <li
+        class="flex items-center space-x-4 text-xs md:text-sm text-accent-100/70"
+      >
+        <span class="text-gray-400">/</span>
         <AppLink
           rel="noopener noreferrer"
           :to="{
             name: 'services.category',
             params: { category: treatment.category[0].slug },
           }"
-          class="flex items-center px-1 hover:underline"
+          class="flex items-center pr-1 hover:underline"
           >{{ treatment.category[0].name }}</AppLink
         >
       </li>
-      <li class="flex items-center space-x-1 text-sm">
+      <li class="flex items-center space-x-4 text-xs md:text-sm">
         <span class="text-gray-400">/</span>
-        <span class="flex items-center px-1">
+        <span class="flex items-center pr-1">
           {{ treatment.name }}
         </span>
       </li>
