f196bdb3c7289e8b76f0a04d9b79cc862dd18932
omgzsa <zselloandras@gmail.com>
2023-04-17 22:08:27 +0200

    employee view page

--- frontend/src/views/EmployeesView.vue
+++ frontend/src/views/EmployeesView.vue
@@ -1,17 +1,24 @@
 <script setup>
 import { ref } from "vue";
+import EmployeeCard from "../components/employee/EmployeeCard.vue";
+
 import employeeData from "../assets/coworkers.json";
 
 const employees = ref(employeeData);
 </script>
 
 <template>
-  <div>
-    <h1>Employees</h1>
-    <ul>
-      <li v-for="employee in employees" :key="employee.id">
-        {{ employee.name }}
-      </li>
-    </ul>
-  </div>
+  <section>
+    <div class="flex flex-col space-y-8 mx-auto">
+      <AppHeader>
+        <template #subtitle> elysia laser clinic </template>
+        <template #title> Munkatársaink </template>
+      </AppHeader>
+      <div
+        class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-y-12 gap-x-8 items-start max-w-screen-xl mx-auto px-2 md:px-4"
+      >
+        <EmployeeCard v-for="item in employees" :key="item.id" :item="item" />
+      </div>
+    </div>
+  </section>
 </template>
