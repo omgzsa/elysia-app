348d013f72e9a23e2e528aeca146d49954cd9643
omgzsa <zselloandras@gmail.com>
2023-04-17 22:08:56 +0200

    global appheader

--- frontend/src/views/ServiceSingleView.vue
+++ frontend/src/views/ServiceSingleView.vue
@@ -1,9 +1,9 @@
 <script setup>
 import { ref, provide, computed } from "vue";
-import { useGetImageUrl } from "../composables/getImageUrl";
 import treatmentsData from "@/assets/treatments.json";
 
 import ServiceTreatmentCard from "@/components/treatment/ServiceTreatmentCard.vue";
+import AppHeader from "../components/AppHeader.vue";
 
 const props = defineProps({
   slug: {
@@ -24,21 +24,14 @@ provide("treatment", treatment);
 <template>
   <section>
     <div class="flex flex-col space-y-8 mx-auto">
-      <span
-        aria-label="háttérkép az elysia felnagyított logójával"
-        role="img"
-        class="pt-20 pb-10 text-center lg:pt-32 lg:pb-16 bg-right lg:text-left md:bg-cover md:bg-center bg-no-repeat"
-        :style="{
-          backgroundImage: `url(${useGetImageUrl(
-            'szolgaltatas-header-bg.webp'
-          )})`,
-        }"
-      >
-        <div class="space-y-1 max-w-screen-xl mx-auto site-padding">
-          <span class="subheading">{{ treatment.category[0].name }}</span>
-          <h2>{{ treatment.name }}</h2>
-        </div>
-      </span>
+      <AppHeader>
+        <template #subtitle>
+          {{ treatment.category[0].name }}
+        </template>
+        <template #title>
+          {{ treatment.name }}
+        </template>
+      </AppHeader>
     </div>
     <div>
       <ServiceTreatmentCard :key="treatment.id" :treatment="treatment" />
