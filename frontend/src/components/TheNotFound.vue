8deafa2c538b9e72f8b7460a7b54784d4f2ea7a6
omgzsa <zselloandras@gmail.com>
2023-04-17 10:29:50 +0200

    not found page adjustments

--- frontend/src/components/TheNotFound.vue
+++ frontend/src/components/TheNotFound.vue
@@ -1,17 +1,35 @@
+<script setup>
+import { useRouter } from "vue-router";
+
+const router = useRouter();
+
+const goBack = () => {
+  router.back();
+};
+</script>
+
 <template>
   <div
     class="grid min-h-screen site-padding bg-white place-content-center gap-4"
   >
-    <h1 class="tracking-widest font-normal text-gray-500 uppercase">
+    <h1 class="tracking-widest font-normal text-gray-300 uppercase">
       404 | Not Found
     </h1>
     <div>
-      <p class="text-gray-500">
+      <span class="text-gray-800">
         Térj vissza a
-        <AppLink class="underline underline-offset-4" :to="{ name: 'home' }"
+        <AppLink
+          class="underline underline-offset-4 hover:text-primary-100"
+          :to="{ name: 'home' }"
           >kezdőlapra</AppLink
         >
-      </p>
+        vagy ugorj az
+        <span
+          class="cursor-pointer underline underline-offset-4 hover:text-primary-100"
+          @click="goBack"
+          >előző oldal</span
+        >ra.
+      </span>
     </div>
   </div>
 </template>
