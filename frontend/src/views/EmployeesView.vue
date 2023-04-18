273d949688800e0d3163782403a82ef3b2599c2f
omgzsa <zselloandras@gmail.com>
2023-04-18 16:08:43 +0200

    changed grid column numbers

--- frontend/src/views/EmployeesView.vue
+++ frontend/src/views/EmployeesView.vue
@@ -1,10 +1,10 @@
 <script setup>
-import { ref } from "vue";
+// import { ref } from "vue";
 import EmployeeCard from "../components/employee/EmployeeCard.vue";
 
 import employeeData from "../assets/coworkers.json";
 
-const employees = ref(employeeData);
+// const employees = ref(employeeData);
 </script>
 
 <template>
@@ -15,9 +15,13 @@ const employees = ref(employeeData);
         <template #title> Munkatársaink </template>
       </AppHeader>
       <div
-        class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-y-12 gap-x-8 items-start max-w-screen-xl mx-auto px-2 md:px-4"
+        class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-y-12 gap-x-8 items-start max-w-screen-xl mx-auto px-2 md:px-4"
       >
-        <EmployeeCard v-for="item in employees" :key="item.id" :item="item" />
+        <EmployeeCard
+          v-for="item in employeeData"
+          :key="item.id"
+          :item="item"
+        />
       </div>
     </div>
   </section>
