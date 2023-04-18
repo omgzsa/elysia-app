4409ca2972007100f75d837d15c1c59f57f60878
omgzsa <zselloandras@gmail.com>
2023-04-18 16:07:19 +0200

    computed props for conditional render

--- frontend/src/components/employee/EmployeeDetails.vue
+++ frontend/src/components/employee/EmployeeDetails.vue
@@ -1,24 +1,38 @@
 <script setup>
+import { computed } from "vue";
 import { useGetEmployeeImageUrl } from "../../composables/getEmployeeImageUrl";
 import IconCalendar from "../icons/IconCalendar.vue";
 
-defineProps({
+const props = defineProps({
   item: {
     type: Object,
     required: true,
   },
 });
 
+const hasAppointment = computed(() => {
+  return props.item.appointmentLink;
+});
+
+// a function that returns if item.consultTime is not empty
+const hasConsultTime = computed(() => {
+  return props.item.consultTime.length > 0;
+});
+
+const hasSpecialties = computed(() => {
+  return props.item.specialties.length > 0;
+});
+
 // function that logs to the console
 // const log = (msg) => console.log(msg);
-// log(props.item);
+// log(hasAppointment);
 </script>
 <template>
   <div class="site-padding">
-    <div class="grid py-6 gap-6 md:grid-cols-2">
+    <div class="grid py-6 gap-4 md:grid-cols-2">
       <div class="flex flex-col space-y-6 py-10 order-2 md:order-1">
         <p class="whitespace-pre-wrap">{{ item.bio }}</p>
-        <h3>Szakterületek</h3>
+        <h3 v-if="hasSpecialties">Szakterületek</h3>
         <ul class="list-disc list-inside marker:bg-accent-100">
           <li v-for="item in item.specialties" :key="item.id">
             {{ item.title }}
@@ -27,17 +41,15 @@ defineProps({
       </div>
       <div class="mb-auto md:order-2 mx-auto text-center relative group">
         <div
-          class="bg-primary-100/20 h-96 w-full absolute -bottom-5 rounded-xl -z-10 group-hover:translate-y-1 transition-transform duration-200"
+          class="bg-primary-100/20 h-80 md:h-96 w-full absolute -bottom-5 rounded-xl -z-10 group-hover:translate-y-1 transition-transform duration-200"
         ></div>
-        <div
-          class="flex flex-col space-y-4 px-6 lg:px-12 lg:py-8 group-hover:-translate-y-1 transition-transform duration-200"
-        >
+        <div class="flex flex-col space-y-4 px-6 lg:px-8 lg:py-4">
           <img
             :src="useGetEmployeeImageUrl(item.image)"
             :alt="item.title"
-            class="lg:-mt-32 rounded-xl w-full h-72 object-cover object-top mb-4"
+            class="lg:-mt-24 rounded-xl w-full h-64 md:h-80 object-cover object-top mb-4 group-hover:-translate-y-1 transition-transform duration-200"
           />
-          <h3 class="text-center">Rendelési idő</h3>
+          <h3 v-if="hasConsultTime" class="text-center">Rendelési idő</h3>
           <ul class="pb-2">
             <li
               v-for="consulting in item.consultTime"
@@ -50,9 +62,10 @@ defineProps({
               </div>
             </li>
           </ul>
-          <AppLink to="http://blackand.hu">
+          <AppLink :to="item.appointmentLink">
             <button
               class="rounded-md px-4 py-2 overflow-hidden relative group cursor-pointer font-medium text-white bg-accent-100 border border-accent-100"
+              :class="{ disabled: !hasAppointment }"
             >
               <span
                 class="absolute w-64 h-0 transition-all duration-300 origin-center rotate-45 -translate-x-20 bg-white top-1/2 group-hover:h-64 group-hover:-translate-y-32 ease"
