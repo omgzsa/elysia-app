c7036c3067a00c414a779ce20761c780f4b581d9
omgzsa <zselloandras@gmail.com>
2023-04-19 21:02:19 +0200

    added/imported related doctors

--- frontend/src/components/treatment/ServiceTreatmentCard.vue
+++ frontend/src/components/treatment/ServiceTreatmentCard.vue
@@ -1,6 +1,8 @@
 <script setup>
 import { computed } from "vue";
+import { useGetImageUrl } from "../../composables/getImageUrl";
 import TheBreadCrumbs from "@/components/TheBreadCrumbs.vue";
+import ServiceRelatedDoctorsCard from "@/components/treatment/ServiceRelatedDoctorsCard.vue";
 
 // icons
 import IconLoop from "@/components/icons/IconLoop.vue";
@@ -9,6 +11,8 @@ import IconClock from "@/components/icons/IconClock.vue";
 // const hasTreatTime = computed(() => {
 //   return props.treatment.treatTime;
 // });
+
+const bgImage = "CTA_bg.webp";
 
 const hasData = computed(() => {
   return props.treatment.treatTime || props.treatment.frequency;
@@ -26,68 +30,125 @@ const props = defineProps({
 });
 </script>
 <template>
-  <section class="max-w-screen-xl">
-    <!-- breadcrumb component -->
-    <TheBreadCrumbs />
-    <div class="grid grid-cols-1 sm:grid-cols-3 space-y-2 sm:space-y-0 gap-y-6">
-      <!-- treatment information -->
-      <div class="site-padding order-2 sm:col-span-2 space-y-6">
-        <h3>{{ treatment.content.title }}</h3>
-        <p class="whitespace-pre-wrap">
-          {{ treatment.content.description }}
-        </p>
-        <h3>{{ treatment.content.title2 }}</h3>
-        <p class="whitespace-pre-wrap">{{ treatment.content.description2 }}</p>
-        <h3>{{ treatment.content.title3 }}</h3>
-        <p class="whitespace-pre-wrap">{{ treatment.content.description3 }}</p>
-        <h3>{{ treatment.content.benefitsTitle }}</h3>
-        <ul role="list" class="marker:text-accent-100 list-disc list-inside">
-          <li v-for="item in treatment.content.benefits" :key="item.id">
-            {{ item.text }}
-          </li>
-        </ul>
-      </div>
-      <!-- treatment time/frequency -->
-      <div class="px-4 relative sm:order-2 sm:col-span-1" v-if="hasData">
-        <div
-          class="flex flex-col space-y-6 mx-auto w-64 sm:w-full lg:max-w-md bg-primary-200 py-4 px-4 rounded-xl lg:py-6 lg:px-6 xl:py-8 xl:px-10 sm:sticky top-20"
-        >
+  <section>
+    <div class="max-w-screen-xl mx-auto">
+      <!-- breadcrumb component -->
+      <TheBreadCrumbs />
+      <div
+        class="grid grid-cols-1 sm:grid-cols-3 space-y-2 sm:space-y-0 gap-y-6 mb-8"
+      >
+        <!-- treatment information -->
+        <div class="site-padding order-2 sm:col-span-2 space-y-6">
+          <h3>{{ treatment.content.title }}</h3>
+          <p class="whitespace-pre-wrap">
+            {{ treatment.content.description }}
+          </p>
+          <h3>{{ treatment.content.title2 }}</h3>
+          <p class="whitespace-pre-wrap">
+            {{ treatment.content.description2 }}
+          </p>
+          <h3>{{ treatment.content.title3 }}</h3>
+          <p class="whitespace-pre-wrap">
+            {{ treatment.content.description3 }}
+          </p>
+          <h3>{{ treatment.content.benefitsTitle }}</h3>
+          <ul role="list" class="marker:text-accent-100 list-disc list-inside">
+            <li v-for="item in treatment.content.benefits" :key="item.id">
+              {{ item.text }}
+            </li>
+          </ul>
+        </div>
+        <!-- treatment time/frequency -->
+        <div class="px-4 relative sm:order-2 sm:col-span-1" v-if="hasData">
           <div
-            class="flex items-center space-x-2 sm:flex-col sm:items-start sm:space-x-0 sm:space-y-2 md:flex-row md:items-center md:space-x-2 md:space-y-0"
+            class="flex flex-col space-y-6 mx-auto w-64 sm:w-full lg:max-w-md bg-primary-200 py-4 px-4 rounded-xl lg:py-6 lg:px-6 xl:py-8 xl:px-10 sm:sticky top-20"
           >
-            <IconClock
-              class="text-accent-100 flex-shrink text-xs lg:text-base"
-            />
-            <div class="flex flex-col flex-1">
-              <span
-                class="text-xs lg:text-sm xl:text-base tracking-wider uppercase text-gray-800"
-                >a kezelés időtartama</span
-              >
-              <span
-                class="text-accent-100 font-medium md:font-semibold text-sm"
-                >{{ props.treatment.treatTime }}</span
-              >
+            <div
+              class="flex items-center space-x-2 sm:flex-col sm:items-start sm:space-x-0 sm:space-y-2 md:flex-row md:items-center md:space-x-2 md:space-y-0"
+            >
+              <IconClock
+                class="text-accent-100 flex-shrink text-xs lg:text-base"
+              />
+              <div class="flex flex-col flex-1">
+                <span
+                  class="text-xs lg:text-sm xl:text-base tracking-wider uppercase text-gray-800"
+                  >a kezelés időtartama</span
+                >
+                <span
+                  class="text-accent-100 font-medium md:font-semibold text-sm"
+                  >{{ props.treatment.treatTime }}</span
+                >
+              </div>
             </div>
-          </div>
-          <div
-            v-if="hasFrequency"
-            class="flex items-center space-x-2 sm:flex-col sm:items-start sm:space-x-0 sm:space-y-2 md:flex-row md:items-center md:space-x-2 md:space-y-0"
-          >
-            <IconLoop
-              class="text-accent-100 flex-shrink text-xs lg:text-base"
-            />
-            <div class="flex flex-col flex-1">
-              <span
-                class="text-xs lg:text-sm xl:text-base tracking-wider uppercase text-gray-800"
-                >gyakorisága</span
-              >
-              <span
-                class="text-accent-100 font-medium md:font-semibold text-sm"
-                >{{ props.treatment.frequency }}</span
-              >
+            <div
+              v-if="hasFrequency"
+              class="flex items-center space-x-2 sm:flex-col sm:items-start sm:space-x-0 sm:space-y-2 md:flex-row md:items-center md:space-x-2 md:space-y-0"
+            >
+              <IconLoop
+                class="text-accent-100 flex-shrink text-xs lg:text-base"
+              />
+              <div class="flex flex-col flex-1">
+                <span
+                  class="text-xs lg:text-sm xl:text-base tracking-wider uppercase text-gray-800"
+                  >gyakorisága</span
+                >
+                <span
+                  class="text-accent-100 font-medium md:font-semibold text-sm"
+                  >{{ props.treatment.frequency }}</span
+                >
+              </div>
             </div>
           </div>
         </div>
+      </div>
+    </div>
+    <!-- FAQ call to action -->
+    <div>
+      <div
+        :style="{ backgroundImage: `url(${useGetImageUrl(bgImage)})` }"
+        class="text-center py-20 bg-cover 3xl:bg-contain bg-left bg-no-repeat px-2 space-y-6"
+      >
+        <p
+          class="text-white font-bold text-xl lg:text-2xl xl:text-3xl max-w-screen-md mx-auto"
+        >
+          Kérdése van?
+        </p>
+        <p class="text-white max-w-2xl mx-auto">
+          Böngéssze át a Gyakran Ismételt Kérdéseket, amelyek az orvosainknál
+          felmerülhetnek. Olvashat a hozzájuk tartozó vizsgálatokról és arról
+          mikor érdemes szakemberhez fordulni.
+        </p>
+        <div>
+          <div class="flex flex-col justify-center gap-2 md:flex-row">
+            <AppLink :to="{ name: 'faq.show' }">
+              <button
+                class="rounded-md px-8 py-2 m-1 overflow-hidden relative group cursor-pointer font-medium bg-secondary-100 border border-secondary-100"
+              >
+                <span
+                  class="absolute w-64 h-0 transition-all duration-300 origin-center rotate-45 -translate-x-44 bg-white top-1/2 group-hover:h-64 group-hover:-translate-y-32 ease"
+                ></span>
+                <span
+                  class="font-bold flex flex-row gap-2 relative text-gray-800 group-hover:text-secondary-100 transition duration-300 ease"
+                  >GY.I.K.</span
+                >
+              </button>
+            </AppLink>
+          </div>
+        </div>
+      </div>
+    </div>
+    <!-- related Doctors -->
+    <div class="max-w-screen-xl mx-auto py-8 site-padding space-y-6">
+      <div class="space-y-2 mb-8">
+        <span class="subheading">elysia laser clinic</span>
+        <h3>A témában jártas munkatársaink:</h3>
+      </div>
+      <div class="flex flex-col sm:flex-row gap-12 items-center">
+        <ServiceRelatedDoctorsCard
+          v-for="item in props.treatment.relatedDoctor"
+          :key="item.id"
+          :item="item"
+        />
       </div>
     </div>
   </section>
