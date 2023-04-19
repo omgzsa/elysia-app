6c719646e67fca16c7082233e6a0293d151a1c92
omgzsa <zselloandras@gmail.com>
2023-04-19 16:22:03 +0200

    image update, parallax issue still up

--- frontend/src/components/TheParallax.vue
+++ frontend/src/components/TheParallax.vue
@@ -1,13 +1,13 @@
 <script setup>
 import { useGetImageUrl } from "../composables/getImageUrl";
 
-const image = "contact_bg.webp";
+const image = "elysia-bg.webp";
 </script>
 
 <template>
   <section>
     <div
-      class="flex items-center justify-center h-72 md:h-[600px] m-auto mt-6 bg-scroll bg-center bg-[length:960px_349px] bg-no-repeat md:bg-[length:1440px_524px] xl:bg-[length:1920px_698px]"
+      class="flex items-center justify-center h-72 md:h-[600px] m-auto mt-6 bg-scroll xl:bg-fixed bg-center bg-no-repeat bg-cover"
       :style="{ backgroundImage: `url(${useGetImageUrl(image)})` }"
     ></div>
   </section>
