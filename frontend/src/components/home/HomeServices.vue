585f3e2b2403311b9e1efa7bc5faea34ee6efb9f
omgzsa <zselloandras@gmail.com>
2023-04-20 11:32:04 +0200

    added Z0 skin static emblem

--- frontend/src/components/home/HomeServices.vue
+++ frontend/src/components/home/HomeServices.vue
@@ -29,7 +29,7 @@ const servicesFeatured = computed(() => {
       </p>
     </div>
     <ul
-      class="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 gap-y-8 lg:gap-y-12 px-2 items-start"
+      class="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-5 gap-y-8 lg:gap-y-12 px-2 items-start"
       name="list"
     >
       <li
@@ -51,6 +51,25 @@ const servicesFeatured = computed(() => {
           />
           <p class="font-semibold text-center text-xs sm:text-base">
             {{ service.name }}
+          </p>
+        </AppLink>
+      </li>
+      <li
+        class="list-item transform transition duration-300 ease-in-out hover:scale-105"
+      >
+        <AppLink
+          class="flex flex-col items-center gap-4"
+          :to="{
+            name: 'zero.skin',
+          }"
+        >
+          <img
+            class="h-24 sm:h-32 w-full object-contain"
+            :src="useGetImageUrl('z0_skin_health.webp')"
+            alt=""
+          />
+          <p class="font-semibold text-center text-xs sm:text-base">
+            Z0 Skin Health
           </p>
         </AppLink>
       </li>
