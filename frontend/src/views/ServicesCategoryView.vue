df2722010ae91437ba8e4e333158e72dad7cfdc1
omgzsa <zselloandras@gmail.com>
2023-04-22 14:59:29 +0200

    added site-padding to list

--- frontend/src/views/ServicesCategoryView.vue
+++ frontend/src/views/ServicesCategoryView.vue
@@ -25,7 +25,7 @@ const service = computed(() => {
         </template>
       </AppHeader>
       <div
-        class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-y-12 gap-x-8 items-start max-w-screen-xl mx-auto px-2 md:px-4"
+        class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-y-12 gap-x-8 items-start max-w-screen-xl mx-auto site-padding"
       >
         <CategoryCard
           v-for="item in service.submenu"
