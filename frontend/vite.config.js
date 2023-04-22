dc56aeac841112008efd8bc48771db736980db56
omgzsa <zselloandras@gmail.com>
2023-04-22 23:09:17 +0200

    config test

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,17 +3,30 @@ import { fileURLToPath, URL } from "node:url";
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 
+// const isProduction = import.meta.env.PROD;
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  server: {
-    watch: {
-      usePolling: true,
-    },
-  },
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
     },
+  },
+  server: {
+    port: 3000,
+    watch: {
+      usePolling: true,
+    },
+  },
+  build: {
+    outDir: "dist",
+    assetsDir: "assets",
+    sourcemap: true,
+    // terserOptions: {
+    //   compress: {
+    //     drop_console: import.meta.env.MODE === "production",
+    //   },
+    // },
   },
 });
