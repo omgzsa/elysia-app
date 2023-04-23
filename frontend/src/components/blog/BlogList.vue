62dfc6070e7d0b7e4ce8b0e3516974192306f92f
omgzsa <zselloandras@gmail.com>
2023-04-23 22:46:42 +0200

    update with blogsData.json

--- frontend/src/components/blog/BlogList.vue
+++ frontend/src/components/blog/BlogList.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { ref } from "vue";
+import blogsData from "@/assets/blogs.json";
 import BlogListItem from "./BlogListItem.vue";
 
 defineProps({
@@ -8,27 +8,6 @@ defineProps({
     required: true,
   },
 });
-
-const blogs = ref([
-  {
-    id: 1,
-    title: "Lorem ipsum dolor sit amet",
-    image: "blog-img-1.webp",
-    text: "Lorem ipsum dolor sit amet consectetur. Magna faucibus sed est facilisis elementum.",
-  },
-  {
-    id: 2,
-    title: "Lorem ipsum ",
-    image: "blog-img-2.webp",
-    text: "Lorem ipsum dolor sit amet consectetur. Magna faucibus sed est facilisis elementum. Mauris enim fringilla diam ultricies suspendisse purus elit.",
-  },
-  {
-    id: 3,
-    title: "Lorem ipsum dolor",
-    image: "blog-img-3.webp",
-    text: "Lorem ipsum dolor sit amet consectetur. Magna faucibus sed est facilisis elementum. Mauris enim fringilla diam ultricies suspendisse purus elit. Lorem ipsum dolor sit amet consectetur.",
-  },
-]);
 </script>
 <template>
   <section class="max-w-screen-xl site-padding space-y-12">
@@ -40,7 +19,7 @@ const blogs = ref([
       class="grid justify-center grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3"
     >
       <BlogListItem
-        v-for="blog in blogs"
+        v-for="blog in blogsData"
         :key="blog.id"
         :blog="blog"
         class="mx-auto"
--- frontend/src/components/blog/BlogListItem.vue
+++ frontend/src/components/blog/BlogListItem.vue
@@ -1,27 +1,22 @@
 <script setup>
-import { useGetImageUrl } from "../../composables/getImageUrl";
+import { useGetBlogImageUrl } from "../../composables/getBlogImageUrl";
 
 defineProps({
   blog: {
     type: Object,
     required: true,
   },
-  // image: {
-  //   type: String,
-  //   required: true,
-  // },
 });
 </script>
 <template>
-  <a
-    rel="noopener noreferrer"
-    href="#"
+  <AppLink
+    :to="{ name: 'blog.single', params: { slug: blog.slug } }"
     class="max-w-xs sm:max-w-sm mx-auto group hover:no-underline focus:no-underline border border-gray-400 rounded-xl"
   >
     <img
       role="presentation"
       class="object-cover w-full rounded-t-xl h-44 dark:bg-gray-500"
-      :src="useGetImageUrl(blog.image)"
+      :src="useGetBlogImageUrl(blog.image)"
     />
     <div class="p-6 space-y-2">
       <h3
@@ -29,10 +24,10 @@ defineProps({
       >
         {{ blog.title }}
       </h3>
-      <span class="text-xs text-accent-100 tracking-wide"
-        >January 21, 2021</span
-      >
-      <p>{{ blog.text.slice(0, 120) }}..</p>
+      <span class="text-xs text-accent-100 tracking-wide">{{
+        blog.createdAt
+      }}</span>
+      <p>{{ blog.content.text1.slice(0, 120) }}..</p>
     </div>
-  </a>
+  </AppLink>
 </template>
