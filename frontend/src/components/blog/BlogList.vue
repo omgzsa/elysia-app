20790c5489fe172cf0152dd554f96b330699aa42
omgzsa <zselloandras@gmail.com>
2023-04-24 17:18:55 +0200

    removed button to blogs

--- frontend/src/components/blog/BlogList.vue
+++ frontend/src/components/blog/BlogList.vue
@@ -25,19 +25,5 @@ defineProps({
         class="mx-auto"
       />
     </div>
-
-    <!-- <AppLink :to="{ name: 'blogs.show' }">
-      <button
-        class="rounded-md px-8 py-2 m-1 overflow-hidden relative group cursor-pointer font-medium text-white bg-accent-100 border border-accent-100"
-      >
-        <span
-          class="absolute w-64 h-0 transition-all duration-300 origin-center rotate-45 -translate-x-20 bg-white top-1/2 group-hover:h-64 group-hover:-translate-y-32 ease"
-        ></span>
-        <span
-          class="font-bold flex flex-row gap-2 relative text-white transition duration-300 group-hover:text-accent-100 ease"
-          >További bejegyzések</span
-        >
-      </button>
-    </AppLink> -->
   </section>
 </template>
