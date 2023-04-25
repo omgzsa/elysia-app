1753c6ccf5a9d1822530648c674c397c786acf2f
omgzsa <zselloandras@gmail.com>
2023-04-25 17:30:04 +0200

    adjusted animation delay

--- frontend/src/components/home/HomeIntroduction.vue
+++ frontend/src/components/home/HomeIntroduction.vue
@@ -23,7 +23,7 @@ defineProps({
         height="128"
         width="32"
         v-motion-slide-visible-once-bottom
-        :delay="700"
+        :delay="500"
         class="w-full row-start-2 row-span-3 col-start-2 col-span-4 rounded-xl"
         src="../../assets/images/bemutatkozas-1.webp"
         alt="szájfeltöltés fecskendővel"
@@ -32,7 +32,7 @@ defineProps({
         height="46"
         width="164"
         v-motion-slide-visible-once-bottom
-        :delay="500"
+        :delay="300"
         class="w-full row-start-1 row-span-3 col-start-6 col-span-6 mb-12 sm:mb-16 md:mb-12 lg:mb-14 rounded-xl"
         src="../../assets/images/bemutatkozas-2.webp"
         alt="narancsbőr kezelése"
@@ -41,7 +41,7 @@ defineProps({
         height="64"
         width="256"
         v-motion-slide-visible-once-bottom
-        :delay="300"
+        :delay="100"
         class="w-full row-start-3 row-span-3 col-start-6 col-span-7 rounded-xl"
         src="../../assets/images/bemutatkozas-3.webp"
         alt="arckezelés"
