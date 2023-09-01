260846ef854067225e91f88b2ecedd7b316b5b58
omgzsa <zselloandras@gmail.com>
2023-09-01 16:58:32 +0200

    docs: updated with health care partners

--- frontend/src/views/PricesView.vue
+++ frontend/src/views/PricesView.vue
@@ -34,7 +34,7 @@ useHead({
 
 <template>
   <section>
-    <div class="flex flex-col space-y-8 mx-auto">
+    <div class="flex flex-col mx-auto space-y-8">
       <AppHeader>
         <template #title>Áraink</template>
       </AppHeader>
@@ -48,7 +48,7 @@ useHead({
         class="rounded-xl bg-secondary-200"
       >
         <h3
-          class="rounded-t-xl p-6"
+          class="p-6 rounded-t-xl"
           :class="
             pricesData.indexOf(item) % 2 === 0
               ? 'bg-primary-300'
@@ -61,7 +61,7 @@ useHead({
           <div
             v-for="treatment in item.treatments"
             :key="treatment.id"
-            class="flex flex-col sm:flex-row sm:items-center pb-4 space-y-1"
+            class="flex flex-col pb-4 space-y-1 sm:flex-row sm:items-center"
           >
             <p
               class="max-w-sm lg:max-w-screen-xl"
@@ -73,7 +73,7 @@ useHead({
               {{ treatment.title }}
             </p>
             <div
-              class="hidden sm:block border-b border-b-gray-400 border-dotted flex-grow mx-4"
+              class="flex-grow hidden mx-4 border-b border-dotted sm:block border-b-gray-400"
             ></div>
             <p
               class="font-medium"
@@ -84,11 +84,29 @@ useHead({
               {{ treatment.price }}
             </p>
             <div
-              class="sm:hidden border-b border-b-gray-400 border-dotted flex-grow mx-4"
+              class="flex-grow mx-4 border-b border-dotted sm:hidden border-b-gray-400"
             ></div>
           </div>
         </div>
       </div>
+    </div>
+    <div class="flex flex-col max-w-screen-xl mx-auto space-y-8 site-padding">
+      <p class="text-xs italic">
+        *Az Elysia Lézerközpont szolgáltatásainak díját készpénzben,
+        bankkártyával, illetve bizonyos szolgáltatásokat egészségpénztárral is
+        fizetheti.
+      </p>
+      <p class="text-xs">Egészségpénztári partnereink:</p>
+      <ul>
+        <li class="text-xs">OTP - Országos Egészség- és Önsegélyező Pénztár</li>
+        <li class="text-xs">MBH Gondoskodás Egészségpénztár (régi MKB)</li>
+        <li class="text-xs">
+          Allianz Hungária Egészség- és Önsegélyező Pénztár
+        </li>
+        <li class="text-xs">Generali Egészség- és Önsegélyező Pénztár</li>
+        <li class="text-xs">Prémium Egészség- és Önsegélyező Pénztár</li>
+        <li class="text-xs">Patika Egészségpénztár</li>
+      </ul>
     </div>
   </section>
 </template>
