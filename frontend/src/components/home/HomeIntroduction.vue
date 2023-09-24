0954ca7313c55799d65cf4d39412ba5aa404f1e2
omgzsa <zselloandras@gmail.com>
2023-09-24 21:32:19 +0200

    style: text-left on header and intro content

--- frontend/src/components/home/HomeIntroduction.vue
+++ frontend/src/components/home/HomeIntroduction.vue
@@ -6,10 +6,10 @@ defineProps({
 </script>
 <template>
   <div
-    class="grid grid-cols-1 md:grid-cols-2 gap-x-6 items-center max-w-screen-xl mx-auto site-padding"
+    class="grid items-center max-w-screen-xl grid-cols-1 mx-auto md:grid-cols-2 gap-x-6 site-padding"
   >
     <div
-      class="md:order-1 grid grid-cols-1 gap-y-6 mx-auto text-center md:text-left lg:text-left px-2 max-w-3xl"
+      class="grid max-w-3xl grid-cols-1 px-2 mx-auto md:order-1 gap-y-6"
     >
       <span class="subheading">bemutatkozás</span>
       <h2>{{ title }}</h2>
@@ -18,26 +18,26 @@ defineProps({
       </p>
       <AppLink :to="{ name: 'services' }">
         <button
-          class="mb-8 md:mb-0 rounded-md px-8 py-2 overflow-hidden relative group cursor-pointer font-medium text-white bg-accent-100 border border-accent-100"
+          class="relative px-8 py-2 mb-8 overflow-hidden font-medium text-white border rounded-md cursor-pointer md:mb-0 group bg-accent-100 border-accent-100"
         >
           <span
             class="absolute w-64 h-0 transition-all duration-300 origin-center rotate-45 -translate-x-20 bg-white top-1/2 group-hover:h-64 group-hover:-translate-y-32 ease"
           ></span>
           <span
-            class="font-bold flex flex-row items-center gap-2 relative text-white transition duration-300 group-hover:text-accent-100 ease"
+            class="relative flex flex-row items-center gap-2 font-bold text-white transition duration-300 group-hover:text-accent-100 ease"
             >Szolgáltatásaink</span
           >
         </button>
       </AppLink>
     </div>
 
-    <div class="grid grid-cols-12 grid-rows-5 gap-4 items-center">
+    <div class="grid items-center grid-cols-12 grid-rows-5 gap-4">
       <img
         height="128"
         width="32"
         v-motion-slide-visible-once-bottom
         :delay="500"
-        class="w-full row-start-2 row-span-3 col-start-2 col-span-4 rounded-xl"
+        class="w-full col-span-4 col-start-2 row-span-3 row-start-2 rounded-xl"
         src="../../assets/images/bemutatkozas-1.webp"
         alt="szájfeltöltés fecskendővel"
       />
@@ -46,7 +46,7 @@ defineProps({
         width="164"
         v-motion-slide-visible-once-bottom
         :delay="300"
-        class="w-full row-start-1 row-span-3 col-start-6 col-span-6 mb-12 sm:mb-16 md:mb-12 lg:mb-14 rounded-xl"
+        class="w-full col-span-6 col-start-6 row-span-3 row-start-1 mb-12 sm:mb-16 md:mb-12 lg:mb-14 rounded-xl"
         src="../../assets/images/bemutatkozas-2.webp"
         alt="narancsbőr kezelése"
       />
@@ -55,7 +55,7 @@ defineProps({
         width="256"
         v-motion-slide-visible-once-bottom
         :delay="100"
-        class="w-full row-start-3 row-span-3 col-start-6 col-span-7 rounded-xl"
+        class="w-full col-span-7 col-start-6 row-span-3 row-start-3 rounded-xl"
         src="../../assets/images/bemutatkozas-3.webp"
         alt="arckezelés"
       />
