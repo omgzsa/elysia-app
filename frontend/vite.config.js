2c3bd1998f0d1f60205ba3394b47a10c1efd3097
omgzsa <zselloandras@gmail.com>
2023-12-21 10:38:45 +0100

    fix: removed vite-plugin-pages code

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,39 +1,17 @@
 import vue from "@vitejs/plugin-vue";
-import Pages from "vite-plugin-pages";
-import generateSitemap from "vite-plugin-pages-sitemap";
+// import Pages from "vite-plugin-pages";
+// import generateSitemap from "vite-plugin-pages-sitemap";
 import { fileURLToPath, URL } from "node:url";
 import { defineConfig } from "vite";
 
-import treatmentsData from "./src/assets/treatments.json";
-import employeesData from "./src/assets/coworkers.json";
+// import treatmentsData from "./src/assets/treatments.json";
+// import employeesData from "./src/assets/coworkers.json";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue({
       include: [/\.vue$/, /\.md$/],
-    }),
-    Pages({
-      // onRoutesGenerated: (routes) => generateSitemap({ routes }),
-      onRoutesGenerated: (routes) => {
-        const treatments = treatmentsData;
-        const employees = employeesData;
-        const dynamicTreatmentRoutes = treatments.map((treatment) => {
-          `/szolgaltatasok/${treatment.category[0].slug}/${treatment.slug}`;
-        });
-        const dynamicEmployeeRoutes = employees.map((employee) => {
-          `/munkatarsak/${employee.slug}`;
-        });
-        generateSitemap({
-          routes: [
-            ...routes,
-            ...dynamicTreatmentRoutes,
-            ...dynamicEmployeeRoutes,
-          ],
-        });
-      },
-      filename: "sitemap",
-      readable: true,
     }),
   ],
   base: "/",
