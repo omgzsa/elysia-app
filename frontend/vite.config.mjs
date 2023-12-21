e7be7ecd28ac59a1491f061a7696e61899d8faf8
omgzsa <zselloandras@gmail.com>
2023-12-21 12:27:31 +0100

    chore: update .js to .mjs for vite 5

--- frontend/vite.config.mjs
+++ frontend/vite.config.mjs
@@ -1,13 +1,7 @@
 import vue from "@vitejs/plugin-vue";
-// import Pages from "vite-plugin-pages";
-// import generateSitemap from "vite-plugin-pages-sitemap";
 import { fileURLToPath, URL } from "node:url";
 import { defineConfig } from "vite";
 
-// import treatmentsData from "./src/assets/treatments.json";
-// import employeesData from "./src/assets/coworkers.json";
-
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue({
