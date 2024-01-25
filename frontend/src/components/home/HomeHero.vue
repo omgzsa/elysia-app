16b60f1516c1768b8eb778689c7e4eb5e88bec6a
omgzsa <zselloandras@gmail.com>
2024-01-25 11:05:50 +0100

    docs: extra sentence for prioce validity

--- frontend/src/components/home/HomeHero.vue
+++ frontend/src/components/home/HomeHero.vue
@@ -71,6 +71,9 @@ const getResponsiveImage = (id) => {
               >
             </button>
           </AppLink>
+          <p class="text-sm italic">
+            Áraink február 1-től (ill. néhány tétel március 1-től) aktuálisak.
+          </p>
         </div>
       </div>
     </slide>
