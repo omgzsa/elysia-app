c43a1074ace47100c6e80e2bdc20366a59b9b47a
omgzsa <zselloandras@gmail.com>
2024-10-18 14:25:20 +0200

    chore: install directus/sdk, added plugin

---
+++ src/plugins/directus.js
@@ -0,0 +1,34 @@
+// src/plugins/directus.js
+import { createDirectus, rest, readItems, readItem } from '@directus/sdk';
+
+export const directusPlugin = {
+  install: (app, options) => {
+    const client = createDirectus(options.url).with(rest());
+
+    const directusService = {
+      // Get all blog posts
+      async getBlogs(query = {}) {
+        return await client.request(
+          readItems('Blog', {
+            fields: ['id', 'status', 'datum', 'cim', 'slug', 'leiras', 'kep', 'kategoria'],
+            ...query
+          })
+        );
+      },
+
+      // Get a single blog post
+      async getBlog(id, query = {}) {
+        return await client.request(
+          readItem('Blog', id, {
+            fields: ['id', 'status', 'datum', 'szerzo', 'cim', 'slug', 'szoveg', 'leiras', 'kep', 'kategoria'],
+            ...query
+          })
+        );
+      },
+    };
+
+    app.provide('$directus', directusService);
+  }
+};
+
+export default directusPlugin;
